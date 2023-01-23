afe5f40937bc9aa24ed388128788d848e1687fba
unknown <henrique.ph935@gmail.com>
2023-01-23 11:35:36 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from vetores import vetor
+
+print(30 * "-", "MENU", 30 * "-")
+
+print("1. Vetores")
+print("2. Listas Ligadas")
+
+menu = int(input("Digite a opção desejada: "))
+
+if menu == 1:
+    vetor_teste = vetor.Vetor(0)
+    #vetor_teste.inserir_elemento_final(1)
+    #vetor_teste.inserir_elemento_final(2)
+    #vetor_teste.inserir_elemento_final(1)
+    
+    vetor_teste.inserir_elemento_posicao(1, 0)
+    vetor_teste.inserir_elemento_posicao(2, 1)
+    vetor_teste.inserir_elemento_posicao(3, 2)
+    vetor_teste.inserir_elemento_final(2)
+    
+
+    print(vetor_teste.listar_elemento(0))
+    print(vetor_teste.listar_elemento(1))
+    print(vetor_teste.listar_elemento(2))
+    print(vetor_teste.listar_elemento(3))
+    print(vetor_teste.tamanho_vetor())
+    print(vetor_teste.contem(3))
+    print(vetor_teste)
