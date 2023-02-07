d31811d617e4d7c3c1e2d2172b75037b9e9c5a1e
unknown <henrique.ph935@gmail.com>
2023-02-07 15:58:56 -0300

    Cria função de busca

--- listas_ligadas/listas_ligadas.py
+++ listas_ligadas/listas_ligadas.py
@@ -17,4 +17,18 @@ class ListaLigada:
     
     def tamanho(self):
         no_atual = self.inicio
-        print(no_atual)
+        #print(no_atual)
+        contador = 0
+        while no_atual:
+            contador +=1
+            no_atual = no_atual.proximo
+        return contador
+
+
+    def buscar(self, valor):
+        no_atual = self.inicio
+        while no_atual:
+            if no_atual.valor == valor:
+                return True
+            no_atual = no_atual.proximo
+        return False
